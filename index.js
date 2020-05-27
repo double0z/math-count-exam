f9492f3977fc3f7522d1b5f0ad5c7c424fd27bd2
郑凌凌 <zhengll@gdczkj.com>
2020-05-27 18:06:15 +0800

    改为20以内

--- index.js
+++ index.js
@@ -14,18 +14,17 @@ function run(test) {
 }
 
 //type1 a+b=?
-
-function type1() {
+function type1(max) {
     let a = -1;
     let b = -1;
     let op = '+';
     let answer = undefined;
     do {
-        a = getInt(0, 10);
-        b = getInt(0, 10);
+        a = getInt(0, max);
+        b = getInt(0, max);
         op = getOp();
         answer = run(`${a} ${op} ${b}`);
-    } while (!(0 <= answer && answer <= 10))
+    } while (!(0 <= answer && answer <= max))
 
     return [`${a} ${op} ${b} = ${emptyStr}`, answer];
 }
@@ -38,42 +37,48 @@ function test(output) {
 }
 
 //type1 a+()=b?
-
-function type2() {
+function type2(max) {
     let num = [-1];
     let lastNum = -1;
     let emptyIndex = getInt(0, 2);//0,1
     let op = ['+'];
     let answer = undefined;
     do {
-        num[0] = getInt(0, 10);
-        lastNum = getInt(0, 10);
+        num[0] = getInt(0, max);
+        lastNum = getInt(0, max);
         op[0] = getOp();
 
 
 
         answer = run(`${emptyIndex == 1 ? op : ''}(${lastNum}-(${emptyIndex == 0 ? op : ''}${num[0]}))`);
-    } while (!(0 <= answer && answer <= 10))
+    } while (!(0 <= answer && answer <= max))
 
 
     return [`${emptyIndex == 0 ? emptyStr : num[0]} ${op} ${emptyIndex == 1 ? emptyStr : num[0]} = ${lastNum}`, answer];
 }
 
 
-function page() {
+function page(withAnswer) {
 
 
     let total = 100;
     let numPreLine = 4;
     let items = [];
-    for (var i = 0; i < total; i++) {
-        items.push(run(`type${getInt(1, 3)}()`))
-    }
+    let uniqueItemMap = {};
+    do {
+        const problem = run(`type${getInt(1, 3)}(20)`)
+        if (!uniqueItemMap[problem[0]]) {
+            uniqueItemMap[problem[0]] = true;
+            items.push(problem);
+        } else {
+            console.log(`重复:${JSON.stringify(problem)}`)
+        }
+    } while (items.length < total)
 
 
     let buffer = [`<div class="record">得分： ______________</div>`];
     buffer.push('<table>');
-    for (var i = 0; i < total; i++) {
+    for (var i = 0; i < items.length; i++) {
         if ((i + 1) % numPreLine == 1) {
             buffer.push('<tr>');
         }
@@ -85,19 +90,23 @@ function page() {
     buffer.push('</table>');
     // $("#content").html(buffer.join(''))
 
-    let buffer2 = [`<div class="header">-------------------答案-------------------</div>`];
-    buffer2.push('<table>');
-    for (var i = 0; i < total; i++) {
-        if ((i + 1) % numPreLine == 1) {
-            buffer2.push('<tr>');
+    let buffer2 = [];
+    if (withAnswer) {
+        buffer2.push(`<div class="header">-------------------答案-------------------</div>`);
+        buffer2.push('<table>');
+        for (var i = 0; i < total; i++) {
+            if ((i + 1) % numPreLine == 1) {
+                buffer2.push('<tr>');
+            }
+            let withAnswer = items[i][0].replace(emptyStr, '(&nbsp;' + items[i][1] + '&nbsp;)');
+            buffer2.push(`<td class='exam-item item'>${withAnswer}</td>`);
+            if ((i + 4) % numPreLine == 4) {
+                buffer2.push('</tr>');
+            }
         }
-        let withAnswer = items[i][0].replace(emptyStr, '(&nbsp;' + items[i][1] + '&nbsp;)');
-        buffer2.push(`<td class='exam-item item'>${withAnswer}</td>`);
-        if ((i + 4) % numPreLine == 4) {
-            buffer2.push('</tr>');
-        }
+        buffer2.push('</table>');
     }
-    buffer2.push('</table>');
+
     // $("#content2").html(buffer2.join(''))
 
     return [buffer, buffer2];
@@ -109,7 +118,7 @@ $(function () {
         let copy = $('input').val();
         $('#config').hide();
         for (var i = 0; i < copy; i++) {
-            let p = page();
+            let p = page(false);
             $(document.body).append(p[0].join(''));
             $(document.body).append(p[1].join(''));
         }
